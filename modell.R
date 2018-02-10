ae27e2f773d9ffc6f6a49df5a68d8eeffa65e2cf
Arvid <arvid.ottenberg@nordakademie.de>
2018-02-10 12:47:35 +0100

    resturctured programms

--- modell.R
+++ modell.R
@@ -1,17 +1,31 @@
-#Train Decision Tree
-library(C50)
+#Load Data
 setwd("~/Kaggle/Titanic/Machine-Learning-from-Disaster")
-train <- read.csv("./Data/train_p.csv")
-train$Pclass <- factor(train$Pclass)
-train$TravelsWithPartner <- factor(train$TravelsWithPartner)
+train <- read.csv("./Data/train_p.csv",header = TRUE, fileEncoding = "UTF-8")
+
+#Prepare for model
 train$Survived <- factor(train$Survived)
-treeModel <- C5.0(train[, -c(1:3)], train$Survived, trials = 10)
+train$Pclass <- factor(train$Pclass)
+
+drops <- c("Ticket", "Name","Cabin", "Embarked")
+train <- train[, !(names(train) %in% drops)]
+rm(drops)
+
+#Decision Tree
+library(C50)
+treeModel <- C5.0(train[, -c(1:3)], train$Survived, control = C5.0Control(minCases = 40))
 summary(treeModel)
 
-#Evaluate Decision Tree
+#Load test data
 test <- read.csv("./Data/test_p.csv")
-pred <- predict(treeModel, test[,-c(1:2)])
 
+#Prepare data
+test$Pclass <- factor(test$Pclass)
+drops <- c("Ticket", "Name","Cabin", "Embarked")
+test <- test[, !(names(test) %in% drops)]
+rm(drops)
+
+#Evaluate Model
+pred <- predict(treeModel, test[,-c(1:2)])
 result <- data.frame(PassengerId=test$PassengerId, Survived=pred)
 write.csv(result,file = "./Data/prediction.csv", row.names = FALSE)
 
--- preparation.R
+++ preparation.R
@@ -2,71 +2,62 @@
 setwd("~/Kaggle/Titanic/Machine-Learning-from-Disaster")
 #titanic <- read.csv("./Data/train.csv")
 titanic <- read.csv("./Data/test.csv")
-summary(titanic)
 
 #Extract Title
 library(dplyr)
 titanic$Title <- case_when(
-  grepl("Mrs.",titanic$Name) ~ "Mrs.",
-  grepl("Mr.",titanic$Name) ~ "Mr.",
-  grepl("Miss.",titanic$Name) ~ "Miss.",
-  grepl("Master.",titanic$Name) ~ "Master.",
-  grepl("Dr.",titanic$Name) ~ "Dr.",
-  grepl("Rev.",titanic$Name) ~ "Rev.",
+  grepl("Mrs.",titanic$Name) ~ "Mrs",
+  grepl("Mr.",titanic$Name) ~ "Mr",
+  grepl("Miss.",titanic$Name) ~ "Miss",
+  grepl("Master.",titanic$Name) ~ "Master",
+  grepl("Dr.",titanic$Name) ~ "Dr",
+  grepl("Rev.",titanic$Name) ~ "Rev",
   TRUE ~ "Other"
 )
-titanic$Title <- factor(titanic$Title)
 
 #Extract familyname to match couples
-titanic$Name <- as.character(titanic$Name)
-titanic$FamilyName <- case_when(
-  titanic$Title == "Mrs." ~ gsub("\\(.*", "", titanic$Name),
-  titanic$Title == "Mr." ~ titanic$Name,
-  TRUE ~ ""
-)
-titanic$FamilyName <- gsub("Mrs.","Mr.", titanic$FamilyName)
-titanic$FamilyName <- factor(gsub(" ", "", titanic$FamilyName, fixed = TRUE))
-occurences <- table(unlist(titanic$FamilyName))
-titanic$FamilyNameCount <- occurences[titanic$FamilyName]
-rm(occurences)
-titanic$TravelsWithPartner <- ifelse(titanic$FamilyNameCount == 2,1,0)
-titanic$TravelsWithPartner <- factor(titanic$TravelsWithPartner)
-drops <- c("FamilyName", "FamilyNameCount")
-titanic <- titanic[, !(names(titanic) %in% drops)]
-rm(drops)
+#Pobably bad idea because wife neednt be in the same dataset
+#titanic$Name <- as.character(titanic$Name)
+#titanic$FamilyName <- case_when(
+#  titanic$Title == "Mrs" ~ gsub("\\(.*", "", titanic$Name),
+#  titanic$Title == "Mr" ~ titanic$Name,
+#  TRUE ~ ""
+#)
+#titanic$FamilyName <- gsub("Mrs.","Mr.", titanic$FamilyName)
+#titanic$FamilyName <- factor(gsub(" ", "", titanic$FamilyName, fixed = TRUE))
+#occurences <- table(unlist(titanic$FamilyName))
+#titanic$FamilyNameCount <- occurences[titanic$FamilyName]
+#rm(occurences)
+#titanic$TravelsWithPartner <- ifelse(titanic$FamilyNameCount == 2,1,0)
+#titanic$TravelsWithPartner <- factor(titanic$TravelsWithPartner)
+#drops <- c("FamilyName", "FamilyNameCount")
+#titanic <- titanic[, !(names(titanic) %in% drops)]
+#rm(drops)
 
 #Fill missing Ages with mean age
 titanic$Age[is.na(titanic$Age)] <- mean(titanic$Age, na.rm = TRUE)
 
-#Missing Factor Level
-levels(titanic$Embarked)[1] = "missing"
-
 #Extract additional info from cabin
-titanic$CabinLetter <- substr(titanic$Cabin,1,1)
-titanic$CabinLetter <- factor(titanic$CabinLetter)
-levels(titanic$CabinLetter)[1] = "missing"
-levels(titanic$CabinLetter)
+#titanic$CabinLetter <- substr(titanic$Cabin,1,1)
+#titanic$CabinLetter <- factor(titanic$CabinLetter)
+#levels(titanic$CabinLetter)[1] = "missing"
+#levels(titanic$CabinLetter)
 
-titanic$CabinNumber <- gsub("A|B|C|D|E|F|G|T","", titanic$Cabin)
-titanic$CabinNumber <- as.numeric(titanic$CabinNumber)
+#titanic$CabinNumber <- gsub("A|B|C|D|E|F|G|T","", titanic$Cabin)
+#titanic$CabinNumber <- as.numeric(titanic$CabinNumber)
 
 #Extract additional info from tickets
-titanic$TicketClass <- gsub(" |1|2|3|4|5|6|7|8|9|0|\\.|/","", titanic$Ticket)
-titanic$TicketClass <- sapply(substr(titanic$TicketClass,1,4), tolower)
-titanic$TicketClass <- factor(titanic$TicketClass)
-levels(titanic$TicketClass)[1] = "missing"
-table(titanic$TicketClass)
+#titanic$TicketClass <- gsub(" |1|2|3|4|5|6|7|8|9|0|\\.|/","", titanic$Ticket)
+#titanic$TicketClass <- sapply(substr(titanic$TicketClass,1,4), tolower)
+#titanic$TicketClass <- factor(titanic$TicketClass)
 
-#Prepare for model
-titanic$Pclass <- factor(titanic$Pclass)
-drops <- c("Ticket", "Name","Cabin", "CabinNumber")
-titanic <- titanic[, !(names(titanic) %in% drops)]
-rm(drops)
+#Missing Factor Levels
+levels(titanic$Cabin)[1] = "missing"
+levels(titanic$Embarked)[1] = "missing"
 
 #Save data
 #write.csv(titanic, file = "./Data/train_p.csv")
 write.csv(titanic, file = "./Data/test_p.csv")
-
 
 
 
